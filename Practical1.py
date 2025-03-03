bef753f107714dca88a69291d6005c79b9c4c192
tshering yangki <02240105.cst@rub.edu.bt>
2025-03-04 00:09:04 +0600

    your commit message

--- Practical1.py
+++ Practical1.py
@@ -1,6 +1,6 @@
 studentName = "yangki"
 studentNumber = "02240105"
-print ("My name is " + studentName + " and my student number is " + studentNumber + " ")
+print ("My name is " + studentName + " and my student number is " + studentNumber + ".")
 x=4
 y=3
 print(x*y)
@@ -12,3 +12,7 @@ print(y/z)
 x= "hello"
 y= "world"
 print(x+y)
+ 
+t=5
+u=2
+print(t/u)
